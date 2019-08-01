7b592e0e2b3bebf1cd40204d1808785f27d8ccf9
RafaPinzon93 <rafapinzon_@hotmail.com>
2019-08-01 16:58:31 -0500

    Added channels and redis

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -9,17 +9,54 @@ from .models import Thread, ChatMessage
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print("connected", event)
+
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        thread_obj = await self.get_thread(me, other_user)
+        self.chat_room = f'thread_{thread_obj.id}'
+        await self.channel_layer.group_add(
+            self.chat_room,
+            self.channel_name
+        )
+
         await self.send({
             "type": "websocket.accept"
         })
 
+    async def websocket_receive(self, event):
+        front_text = event.get('text', None)
+        if front_text is not None:
+            loaded_dict_data = json.loads(front_text)
+            msg = loaded_dict_data.get('message')
+            user = self.scope['user']
+            username = 'default'
+            if user.is_authenticated:
+                username = user.username
+            response = {
+                'message': msg,
+                'username': username,
+            }
+            new_event = {
+                "type": "websocket.send",
+                "text": json.dumps(response)
+            }
+            await self.channel_layer.group_send(
+                self.chat_room,
+                {
+                    "type": "chat_message",
+                    "message":  json.dumps(response)
+                }
+            )
+
+    async def chat_message(self, event):
         await self.send({
             "type": "websocket.send",
-            "text": "Hello world"
+            "text": event["message"]
         })
-
-    async def websocket_receive(self, event):
-        print("received", event)
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
+
+    @database_sync_to_async
+    def get_thread(self, user, other_username):
+        return Thread.objects.get_or_new(user, other_username)[0]
